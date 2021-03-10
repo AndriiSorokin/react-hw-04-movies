b403c8e0768ab717a58a84969821675fd83cb6a2
AndriiSorokin <68332371+AndriiSorokin@users.noreply.github.com>
2021-03-10 19:55:39 +0200

    make style movie page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
 import HomeView from './Pages/HomeView';
-import MoviesView from './Pages/MoviesView';
+import MoviesView from './Pages/MovieView/MoviesView';
 import MovieDetailsPage from './components/MovieDetailsPage/MovieDetailsPage';
 import ErrorView from './Pages/ErrorView';
 
--- src/Pages/MovieView/MoviesView.js
+++ src/Pages/MovieView/MoviesView.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory, useLocation, useRouteMatch } from 'react-router-dom';
-import { searchMovie } from '../components/fetch/fetch';
+import { searchMovie } from '../../components/fetch/fetch';
+import style from './MoviesView.module.css';
 // import MovieDetailsPage from '../components/MovieDetailsPage/MovieDetailsPage';
 
 const MoviesView = () => {
@@ -28,10 +29,20 @@ const MoviesView = () => {
         <input onChange={handleInput} name="search" type="text" value={querry} />
         <button type="submit">Search</button>
       </form>
-      <ul>
+      <ul className={style.trendingList}>
         {movies.map(movie => (
-          <li key={movie.id} onClick={goTo}>
-            <h2>{movie.original_title}</h2>
+          <li className={style.trendigItem} key={movie.id} onClick={goTo}>
+            <h2 className={style.trendingTitle}>
+              {movie.title}
+              {movie.original_title}
+            </h2>
+            <img
+              className={style.trendingImg}
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              width="300"
+              height="300"
+              alt="img"
+            />
           </li>
         ))}
       </ul>
